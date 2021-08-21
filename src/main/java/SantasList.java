5e684740e71b8239c85f47b0f6638084c6283d14
bulir <aboandrus@gamil.com>
2021-08-21 14:05:19 +0200

    Implemented code

---
+++ src/main/java/SantasList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SantasList {
+
+    public static List<String> findChildren(List<String> santasList, List<String> children) {
+        Set<String> goodchildren = new HashSet<>();
+        children.forEach(child -> addChiledOnSanatasList(child, goodchildren, santasList) );
+        return goodchildren.stream().sorted().collect(Collectors.toList());
+    }
+
+    public static void addChiledOnSanatasList( String chiled, Set<String> goodchildren, List<String> santasList){
+        if(santasList.contains(chiled)){
+            goodchildren.add(chiled);
+        }
+    }
+
+}
---
+++ src/test/java/SantasListTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SantasListTest {
+
+    @Test
+    public void basicTest() {
+        ArrayList<String> santasList = new ArrayList<String>();
+        ArrayList<String> children = new ArrayList<String>();
+        for(String s : new String[] {"Jason", "Jackson", "Jordan", "Johnny"})
+            santasList.add(s);
+        for(String s : new String[] {"Jason", "Jordan", "Jennifer"})
+            children.add(s);
+        ArrayList<String> goodChildren = new ArrayList<String>();
+
+        for(String s : new String[] {"Jason", "Jordan"}) {
+            goodChildren.add(s);
+        }
+        assertEquals(goodChildren, SantasList.findChildren(santasList, children));
+    }
+
+    @Test
+    public void sortingTest() {
+        ArrayList<String> santasList = new ArrayList<String>();
+        ArrayList<String> children = new ArrayList<String>();
+        for(String s : new String[] {"Jason", "Jackson", "Johnson", "JJ"})
+            santasList.add(s);
+        for(String s : new String[] {"Jason", "James", "JJ"})
+            children.add(s);
+        ArrayList<String> goodChildren = new ArrayList<String>();
+
+        for(String s : new String[] {"JJ", "Jason"}) {
+            goodChildren.add(s);
+        }
+        assertEquals(goodChildren, SantasList.findChildren(santasList, children));
+    }
+
+    @Test
+    public void capitalizationTest() {
+        ArrayList<String> santasList = new ArrayList<String>();
+        ArrayList<String> children = new ArrayList<String>();
+        for(String s : new String[] {"jASon", "JAsoN", "JaSON", "jasON"})
+            santasList.add(s);
+        for(String s : new String[] {"JasoN", "jASOn", "JAsoN", "jASon", "JASON"})
+            children.add(s);
+        ArrayList<String> goodChildren = new ArrayList<String>();
+
+        for(String s : new String[] {"JAsoN", "jASon"}) {
+            goodChildren.add(s);
+        }
+        assertEquals(goodChildren, SantasList.findChildren(santasList, children));
+    }
+}
